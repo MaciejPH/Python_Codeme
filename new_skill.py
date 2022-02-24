b7978e04fce909d27c5f2efcfcf1d2e38f61468b
MaciejPH <Maciek-piech91@wp.pl>
2022-02-24 15:59:02 +0100

    tekst

---
+++ new_skill.py
@@ -0,0 +1,6 @@
+learning_hours = 75
+hours_per_week = 8
+
+weeks = learning_hours / hours_per_week
+
+print('liczba tygodni nauki = ', weeks)
---
+++ triangle.py
@@ -0,0 +1,5 @@
+print("hello world")
+print()
+print("my name is Maciek")
+print("box")
+print("lechia")
